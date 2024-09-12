0215ed76a33fa87f961717216f5c94e553a0571d
Maor Leger <maorleger@microsoft.com>
2024-09-12 13:20:50 -0700

    split up functions

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,38 @@ const client = Translator(
   new DefaultAzureCredential()
 );
 
-async function main() {
+/**
+ * Demonstrates using OpenTelemetry with a wrapper span
+ */
+function withWrapperSpan() {
+  // As an end user I create a wrapper span
   const tracer = trace.getTracer("main");
-  await tracer.startActiveSpan("main", async (span) => {
-    const formats = await client
-      .path("/document/formats")
-      .get({ tracingOptions: { tracingContext: context.active() } });
+  return tracer.startActiveSpan("main", async (span) => {
+    const formats = await client.path("/document/formats").get(); // parent span will be automatically propagated
     if (isUnexpected(formats)) {
       throw new Error("Unexpected response");
     }
 
     span.end();
   });
+}
+
+function withManualSpanPropagation() {
+  // Without a wrapper span, you can still pass the active context
+  return client
+    .path("/document/formats")
+    .get({ tracingOptions: { tracingContext: context.active() } });
+}
+
+function basicTracing() {
+  // Or using automatic span propagation (no need to pass anything)
+  return client.path("/document/formats").get();
+}
+
+async function main() {
+  await basicTracing();
+  await withManualSpanPropagation();
+  await withWrapperSpan();
 }
 
 main().catch((err) => {
--- src/tracing.ts
+++ src/tracing.ts
@@ -1,10 +1,15 @@
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-const { createAzureSdkInstrumentation } = require("@azure/opentelemetry-instrumentation-azure-sdk");
+const {
+  createAzureSdkInstrumentation,
+} = require("@azure/opentelemetry-instrumentation-azure-sdk");
 
 // Set-up and configure a Node Tracer Provider using OpenTelemetry
 const opentelemetry = require("@opentelemetry/api");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/tracing");
+const {
+  SimpleSpanProcessor,
+  ConsoleSpanExporter,
+} = require("@opentelemetry/tracing");
 
 const provider = new NodeTracerProvider();
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
